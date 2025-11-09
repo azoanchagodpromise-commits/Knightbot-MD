6a5c89f85efc4fa548b35bca0391b3202c08dc98
azoanchagodpromise-commits <azoanchagodpromise@gmail.com>
2025-11-09 12:16:57 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
   packname: 'Knight Bot',
-  author: '‎',
+  author: 'POLOG KNIGHT BOT ',
   botName: "Knight Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '237652130158', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "Public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
